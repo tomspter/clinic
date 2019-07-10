b4fad46ce609ef50b4481a19e46961515d9a7c82
tomspter <2501760110@qq.com>
2019-07-10 10:39:28 +0800

    第二次修改

--- src/main/java/com/clinicmaster/clinic/aop/ControllerInterceptor.java
+++ src/main/java/com/clinicmaster/clinic/aop/ControllerInterceptor.java
@@ -1,78 +1,84 @@
-//package com.clinicmaster.clinic.aop;
-//
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.Around;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@Aspect
-//@Component
-//@Slf4j
-//public class ControllerInterceptor {
-//
-//    ThreadLocal<Map<String, Long>> count = new ThreadLocal<>();
-//
-//    /**
-//     * 定义一个切入点. 解释下：
-//     * <p>
-//     * ~ 第一个 * 代表任意修饰符及任意返回值. ~ 第二个 * 定义在web包或者子包 ~ 第三个 * 任意方法 ~ .. 匹配任意数量的参数.
-//     */
-//    static final String pCutStr = "execution(* com.clinicmaster.clinic.controller.callPatient(..))";
-//
-//    @Pointcut(pCutStr)
-//    public void logPoint() {
-//
-//    }
-//
-//    @Around("logPoint()")
-//    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
-//
-//        if (count.get() == null) {
-//            count.set(new HashMap<>(4));
-//        }
-//
-//        Object result = joinPoint.proceed();
-//        if (result == null) {
-//            return null;
-//        }
-//
-//        String tragetClassName = joinPoint.getSignature().getDeclaringTypeName();
-//        String MethodName = joinPoint.getSignature().getName();
-//
-//        Object[] args = joinPoint.getArgs();// 参数
-//        int argsSize = args.length;
-//        String argsTypes = "";
-//        String typeStr = joinPoint.getSignature().getDeclaringType().toString().split(" ")[0];
-//        String returnType = joinPoint.getSignature().toString().split(" ")[0];
-//
-//        log.info("类/接口:" + tragetClassName + "(" + typeStr + ")");
-//        log.info("方法:" + MethodName);
-//        log.info("参数个数:" + argsSize);
-//        log.info("返回类型:" + returnType);
-//
-//        if (argsSize > 0) {
-//            // 拿到参数的类型
-//            for (Object object : args) {
-//                argsTypes += object.getClass().getTypeName() + " ";
-//            }
-//            log.info("参数类型：" + argsTypes);
-//        }
-//
-//        if (count.get().containsKey(MethodName)) {
-//            Long num = count.get().get(MethodName);
-//            count.get().remove(MethodName);//先移除，在增加
-//            count.get().put(MethodName, num + 1);
-//
-//        } else {
-//            count.get().put(MethodName, 1L);
-//        }
-//
-//        return result;
-//    }
-//}
+package com.clinicmaster.clinic.aop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+@Component
+@Slf4j
+public class ControllerInterceptor {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    ThreadLocal<Map<Integer, Integer>> count = new ThreadLocal<>();
+
+    /**
+     * 定义一个切入点. 解释下：
+     * <p>
+     * ~ 第一个 * 代表任意修饰符及任意返回值. ~ 第二个 * 定义在web包或者子包 ~ 第三个 * 任意方法 ~ .. 匹配任意数量的参数.
+     */
+    static final String pCutStr = "execution(* com.clinicmaster.clinic.controller.RegisterController.callPatient(Integer))";
+
+    @Pointcut(value = pCutStr)
+    public void cutPoint() {
+
+    }
+
+
+    @Around("cutPoint()")
+    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        if (count.get()==null) {
+            log.info("new map...");
+            count.set(new HashMap<>(16));
+        }
+
+        Object result = joinPoint.proceed();
+        if (result == null) {
+            return null;
+        }
+
+        Object[] args = joinPoint.getArgs();
+
+        log.info("arg:{}",args[0]);
+
+        if (count.get().containsKey(args[0])) {
+            Integer num = count.get().get(args[0]);
+            count.get().remove(args[0]);//先移除，在增加
+            count.get().put((Integer) args[0], num + 1);
+        } else {
+            count.get().put((Integer) args[0], 1);
+        }
+
+        log.info("num:{}",count.get().get(args[0]));
+
+        return result;
+    }
+
+    @AfterReturning("cutPoint()")
+    public void afterMethod(JoinPoint joinPoint) {
+        //获取参数
+//        Object[] args = joinPoint.getArgs();
+//        log.info("args:{}", args);
+
+        for (Map.Entry<Integer, Integer> entry : count.get().entrySet()) {
+            if (entry.getValue().equals(3)) {
+                String key = "patient" + entry.getKey();
+                String patientId = stringRedisTemplate.opsForList().index(key, 0);
+                stringRedisTemplate.opsForList().leftPop(key);
+                stringRedisTemplate.opsForList().rightPush(key,patientId);
+            }
+        }
+
+    }
+}
